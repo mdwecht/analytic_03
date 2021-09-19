67214e2e0fb45bbd9193ca1e2577f9da327da969
mdwecht <mdwecht@gmail.com>
2021-09-19 18:05:30 -0400

    change analytic field to name field

--- models.py
+++ models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Params(models.Model):
-    analytic = models.CharField(max_length=20)
+    name = models.CharField(max_length=20)
     var_01 = models.IntegerField(default=0)
     var_02 = models.IntegerField(default=0)
     var_03 = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.analytic
+        return self.name
